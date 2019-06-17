00ae692c52d08a0b6bfd36b0f778a2e734744601
samanthkumar25 <samanth2012sam@gmail.com>
2019-06-17 11:03:23 +0530

    Changes0617

--- src/components/Forms/Microservices.tsx
+++ src/components/Forms/Microservices.tsx
@@ -4,15 +4,15 @@ import { LinearProgress } from "material-ui";
 import { LARGE } from "material-ui/utils/withWidth";
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter,Input,Label,Form,FormGroup } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Input, Label, Form, FormGroup } from 'reactstrap';
 
 // export const Microservices: React.StatelessComponent<{}> = () => {
-  const wid = {
-    width: '91%',
-  };
+const wid = {
+  width: '91%',
+};
 
-export class Microservices extends React.Component<any,any,any> {
-  constructor(props:any) {
+export class Microservices extends React.Component<any, any, any> {
+  constructor(props: any) {
     super(props);
 
     this.state = {
@@ -20,7 +20,7 @@ export class Microservices extends React.Component<any,any,any> {
       fields: {},
       errors: {}
     };
-    
+
 
     this.toggle = this.toggle.bind(this);
   }
@@ -34,17 +34,17 @@ export class Microservices extends React.Component<any,any,any> {
     window.scrollTo(0, 0)
   }
 
-  handleValidation(){
+  handleValidation() {
     let fields = this.state.fields;
     let errors = {};
     let formIsValid = true;
 
-    if(!fields["email"]){
+    if (!fields["email"]) {
       formIsValid = false;
       errors["email"] = "Cannot be empty";
     }
 
-    if(typeof fields["email"] !== "undefined"){
+    if (typeof fields["email"] !== "undefined") {
       let lastAtPos = fields["email"].lastIndexOf('@');
       let lastDotPos = fields["email"].lastIndexOf('.');
 
@@ -54,24 +54,24 @@ export class Microservices extends React.Component<any,any,any> {
       }
     }
 
-    this.setState({errors: errors});
+    this.setState({ errors: errors });
     return formIsValid;
   }
 
-  contactSubmit(e){
+  contactSubmit(e) {
     e.preventDefault();
-    if(this.handleValidation()){
+    if (this.handleValidation()) {
       alert("Form submitted");
-    }else{
+    } else {
       alert("Form has errors.")
     }
 
   }
 
-  handleChange(field, e){    		
+  handleChange(field, e) {
     let fields = this.state.fields;
-    fields[field] = e.target.value;        
-    this.setState({fields});
+    fields[field] = e.target.value;
+    this.setState({ fields });
   }
 
   render() {
@@ -88,26 +88,26 @@ export class Microservices extends React.Component<any,any,any> {
         </div>
         {/* Floating Img */}
         <div className="conatiner-fluid px-5">
-<div className="row">
-<div className="btn-groups pFxd">
-<div className="showhim w-25">
-<img src="img/Microservicesmain.jpg" className="w-25" />
-<div className="showme bg-primary px-3 py-2 text-white"><Link className="text-white" to="/microserviceexample">Example of Microservices</Link></div>
-</div>
-<div className="showhim w-25 my-1">
-<img src="img/MSbenefits.png" className="w-25" /> <div className="showme bg-primary px-3 py-2 text-white"><Link className="text-white" to="/microservicebenefit">Benefits of Microservices</Link></div></div>
-<div className="showhim w-25 mb-1">
-<img src="img/MSchallenges.png" className="w-25" /> <div className="showme bg-primary px-3 py-2 text-white"><Link className="text-white" to="/microservicechallenge">
-Challenges of Microservices </Link></div></div>
-<div className="showhim w-25">
-<img src="img/Microservicesmain.jpg" className="w-25" /><div className="showme bg-primary px-3 py-2 text-white"> <Link className="text-white" to="/microservicehelp">How we can help you?</Link></div></div>
-</div>
-</div>
-</div>
+          <div className="row">
+            <div className="w-100 btn-groups pFxd">
+              <div className="showhim w-32">
+                <img src="img/examples.png" className="border w-100 h-70" />
+                <div className="showme bg-primary px-3 py-2 text-white"><Link className="text-white" to="/microserviceexample">Example of Microservices</Link></div>
+              </div>
+              <div className="showhim w-32 my-1">
+                <img src="img/benefits.png" className="w-100 h-70" /> <div className="showme bg-primary px-3 py-2 text-white"><Link className="text-white" to="/microservicebenefit">Benefits of Microservices</Link></div></div>
+              <div className="showhim w-32 mb-1">
+                <img src="img/challenge.jpg" className="w-100 h-70" /> <div className="showme bg-primary px-3 py-2 text-white"><Link className="text-white" to="/microservicechallenge">
+                  Challenges of Microservices </Link></div></div>
+              <div className="showhim w-32">
+                <img src="img/help.jpg" className="w-100 h-70" /><div className="showme bg-primary px-3 py-2 text-white"> <Link className="text-white" to="/microservicehelp">How we can help you?</Link></div></div>
+            </div>
+          </div>
+        </div>
         {/* Floating Img   */}
 
         <div
-          className="px-5 pt-3 lineHeight-24"
+          className="container px-5 pt-3 lineHeight-24"
           data-aos="fade-up"
           data-aos-duration="2000"
         >
@@ -155,7 +155,7 @@ Challenges of Microservices </Link></div></div>
           />{" "}
         </div>
         <div className="pt-3 px-5" data-aos="fade-up" data-aos-duration="2000">
-          <h2 className="py-3">Monolith Vs Microservices</h2>
+          <h2 className="container px-5 py-3">Monolith Vs Microservices</h2>
           <div className="text-center">
             <img
               src="img/monovsmicro.png"
@@ -164,139 +164,139 @@ Challenges of Microservices </Link></div></div>
             />{" "}
           </div>
         </div>
-   <div className="d-flex  px-5 py-4 flex-col ">
+        <div className="container d-flex  px-5 py-4 flex-col ">
           <p>
-            <a className="navlink navfont noLine text-logoblue"  onClick={this.toggle}>
+            <a className="navlink navfont noLine text-logoblue" onClick={this.toggle}>
               <small>Contact&nbsp;Us&nbsp;</small>
             </a>
             to learn more about SYNECTIKS MicroServices.
           </p>
         </div>
-     
+
         {/*  */}
         <div className="">
-  {/* <Button color="primary" onClick={this.toggle}> Contact&nbsp;Us&nbsp; </Button>  */}
-<Modal isOpen={this.state.modal} toggle={this.toggle} >
-  <ModalHeader className="bg-lightgrey" toggle={this.toggle}>Contact Us</ModalHeader>
-  <ModalBody className="bg-lightgrey">
- 
-    <div className="px-5 py-3">
-      <div className="text-center col-md-12  p-2 d-flex flex-col">
-        <div className="p-2 col-sm-6">
-         <input  type="text"  placeholder="&nbsp;&nbsp;First Name" className="inputBoxFlex" width="90%"/>
-        </div>
-        <div className="p-2 col-sm-6">
-          <input className="inputBoxFlex" placeholder="&nbsp;&nbsp;Last Name" width="90%"/>
-        </div>
-        </div>
-        <div className="text-center col-sm-12  p-2 d-flex flex-col">
-        <div className="p-2 col-sm-12">
-        <input className="inputBoxFlex"   style={wid} placeholder="&nbsp;&nbsp;Business Email Address" width="91%"
-        onChange={this.handleChange.bind(this, "email")} value={this.state.fields["email"]}/>
-        </div>
-        </div>
-        <div className="text-center col-md-12  p-2 d-flex flex-col">
-        <div className="p-2 col-sm-6">
-          <input className="inputBoxFlex" placeholder="&nbsp;&nbsp;Company" width="90%"/>
-        </div>
-        <div className="p-2 col-sm-6">
-          <input className="inputBoxFlex" placeholder="&nbsp;&nbsp;Job Title" width="90%"/>
-        </div>
-        </div>
-        <div className="text-center col-md-12  p-2 d-flex flex-col">
-        <div className="p-2 col-sm-6">
-          <input className="inputBoxFlex" placeholder="&nbsp;&nbsp;Country/Region" width="90%"/>
-        </div>
-        <div className="p-2 col-sm-6">
-          <input className="inputBoxFlex" placeholder="&nbsp;&nbsp;Telephone" width="90%"/>
-        </div>
-        </div>
-        <div className="text-center col-md-12  p-2 d-flex flex-col">
-        <div className="p-2 col-sm-6">
-          <select className="inputBoxFlex">
-            <option value="enterprise">
-              &nbsp;Contact&nbsp;Source
-            </option>
-            <option value="enterprise">&nbsp;Advertisement</option>
-            <option value="foundation">
-              &nbsp;Customer&nbsp;Event
-            </option>
-            <option value="migration">
-              &nbsp;Employee&nbsp;Referral
-            </option>
-            <option value="operations">
-              &nbsp;Google&nbsp;Adwords
-            </option>
-            <option value="others">&nbsp;Other</option>
-            <option value="optimization">&nbsp;Partner</option>
-            <option value="optimization">
-              &nbsp;Purchased&nbsp;List
-            </option>
-            <option value="optimization">
-              &nbsp;Trade&nbsp;Show
-            </option>
-            <option value="optimization">&nbsp;Webinar</option>
-            <option value="optimization">&nbsp;Website</option>
-          </select>
-        </div>
-        <div className="p-2 col-sm-6">
-          <select className="inputBoxFlex">
-            <option value="enterprise">
-              &nbsp;Select&nbsp;Service
-            </option>
-            <option value="enterprise">&nbsp;Cloud</option>
-            <option value="enterprise">
-              &nbsp;Enterprise&nbsp;Transformation
-            </option>
-            <option value="foundation">&nbsp;Foundation</option>
-            <option value="migration">
-              &nbsp;Migration&nbsp;&amp;&nbsp;Deployment
-            </option>
-            <option value="operations">&nbsp;Operations</option>
-            <option value="optimization">&nbsp;Optimization</option>
-            <option value="others">&nbsp;Others</option>
-          </select>
-        </div>
-      </div>
-    </div>
+          {/* <Button color="primary" onClick={this.toggle}> Contact&nbsp;Us&nbsp; </Button>  */}
+          <Modal isOpen={this.state.modal} toggle={this.toggle} >
+            <ModalHeader className="bg-lightgrey" toggle={this.toggle}>Contact Us</ModalHeader>
+            <ModalBody className="bg-lightgrey">
 
-  </ModalBody>
-  <ModalFooter className="bg-lightgrey">
-    <div className="text-center p-2" onClick={this.toggle}>
-      <button className="btn bg-logoblue text-white btnSend" id="submit" value="Submit">Submit</button>
-    </div>{" "}
-  </ModalFooter>
-</Modal>
-</div>  
-        
+              <div className="px-5 py-3">
+                <div className="text-center col-md-12  p-2 d-flex flex-col">
+                  <div className="p-2 col-sm-6">
+                    <input type="text" placeholder="&nbsp;&nbsp;First Name" className="inputBoxFlex" width="90%" />
+                  </div>
+                  <div className="p-2 col-sm-6">
+                    <input className="inputBoxFlex" placeholder="&nbsp;&nbsp;Last Name" width="90%" />
+                  </div>
+                </div>
+                <div className="text-center col-sm-12  p-2 d-flex flex-col">
+                  <div className="p-2 col-sm-12">
+                    <input className="inputBoxFlex" style={wid} placeholder="&nbsp;&nbsp;Business Email Address" width="91%"
+                      onChange={this.handleChange.bind(this, "email")} value={this.state.fields["email"]} />
+                  </div>
+                </div>
+                <div className="text-center col-md-12  p-2 d-flex flex-col">
+                  <div className="p-2 col-sm-6">
+                    <input className="inputBoxFlex" placeholder="&nbsp;&nbsp;Company" width="90%" />
+                  </div>
+                  <div className="p-2 col-sm-6">
+                    <input className="inputBoxFlex" placeholder="&nbsp;&nbsp;Job Title" width="90%" />
+                  </div>
+                </div>
+                <div className="text-center col-md-12  p-2 d-flex flex-col">
+                  <div className="p-2 col-sm-6">
+                    <input className="inputBoxFlex" placeholder="&nbsp;&nbsp;Country/Region" width="90%" />
+                  </div>
+                  <div className="p-2 col-sm-6">
+                    <input className="inputBoxFlex" placeholder="&nbsp;&nbsp;Telephone" width="90%" />
+                  </div>
+                </div>
+                <div className="text-center col-md-12  p-2 d-flex flex-col">
+                  <div className="p-2 col-sm-6">
+                    <select className="inputBoxFlex">
+                      <option value="enterprise">
+                        &nbsp;Contact&nbsp;Source
+            </option>
+                      <option value="enterprise">&nbsp;Advertisement</option>
+                      <option value="foundation">
+                        &nbsp;Customer&nbsp;Event
+            </option>
+                      <option value="migration">
+                        &nbsp;Employee&nbsp;Referral
+            </option>
+                      <option value="operations">
+                        &nbsp;Google&nbsp;Adwords
+            </option>
+                      <option value="others">&nbsp;Other</option>
+                      <option value="optimization">&nbsp;Partner</option>
+                      <option value="optimization">
+                        &nbsp;Purchased&nbsp;List
+            </option>
+                      <option value="optimization">
+                        &nbsp;Trade&nbsp;Show
+            </option>
+                      <option value="optimization">&nbsp;Webinar</option>
+                      <option value="optimization">&nbsp;Website</option>
+                    </select>
+                  </div>
+                  <div className="p-2 col-sm-6">
+                    <select className="inputBoxFlex">
+                      <option value="enterprise">
+                        &nbsp;Select&nbsp;Service
+            </option>
+                      <option value="enterprise">&nbsp;Cloud</option>
+                      <option value="enterprise">
+                        &nbsp;Enterprise&nbsp;Transformation
+            </option>
+                      <option value="foundation">&nbsp;Foundation</option>
+                      <option value="migration">
+                        &nbsp;Migration&nbsp;&amp;&nbsp;Deployment
+            </option>
+                      <option value="operations">&nbsp;Operations</option>
+                      <option value="optimization">&nbsp;Optimization</option>
+                      <option value="others">&nbsp;Others</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+            </ModalBody>
+            <ModalFooter className="bg-lightgrey">
+              <div className="text-center p-2" onClick={this.toggle}>
+                <button className="btn bg-logoblue text-white btnSend" id="submit" value="Submit">Submit</button>
+              </div>{" "}
+            </ModalFooter>
+          </Modal>
+        </div>
+
 
         {/* <Button color="secondary" onClick={this.toggle}>Cancel</Button> */}
-        
+
 
         {/* <Button color="secondary" onClick={this.toggleAlert.bind(this)} >Contact&nbsp;Us&nbsp;</Button> */}
 
-        <div className="d-flex justify-content-around align-items-center px-5 pb-3 flex-col text-center col-md-12">
-            <div className="pb-3 col-sm-3" >
+        <div className="  justify-content-around align-items-center px-5 pb-3 flex-col text-center col-md-12 d-none">
+          <div className="pb-3 col-sm-3" >
             <Link to="/microserviceexample">
-                        <button className='btn text-white bg-logoblue  btnLearn  '>Example of Microservices</button>
-                        </Link>
-                    </div>
-                    <div className="pb-3 col-sm-3" >
-                    <Link to="/microservicebenefit">
-                        <button className='btn text-white bg-logoblue  btnLearn   '>Benefits of Microservices</button>
-                        </Link>
-                    </div>
-                    <div className="pb-3 col-sm-3" >
-                    <Link to="/microservicechallenge">
-                        <button className='btn text-white bg-logoblue  btnLearn '>Challenges of Microservices</button>
-                        </Link>
-                    </div>
-                    <div className="pb-3 col-sm-3" >
-                    <Link to="/microservicehelp">
-                        <button className='btn text-white bg-logoblue  btnLearn '>How we can help you?</button>
-                        </Link>
-                    </div>
-                    </div> 
+              <button className='btn text-white bg-logoblue  btnLearn  '>Example of Microservices</button>
+            </Link>
+          </div>
+          <div className="pb-3 col-sm-3" >
+            <Link to="/microservicebenefit">
+              <button className='btn text-white bg-logoblue  btnLearn   '>Benefits of Microservices</button>
+            </Link>
+          </div>
+          <div className="pb-3 col-sm-3" >
+            <Link to="/microservicechallenge">
+              <button className='btn text-white bg-logoblue  btnLearn '>Challenges of Microservices</button>
+            </Link>
+          </div>
+          <div className="pb-3 col-sm-3" >
+            <Link to="/microservicehelp">
+              <button className='btn text-white bg-logoblue  btnLearn '>How we can help you?</button>
+            </Link>
+          </div>
+        </div>
 
         {/* Contact Starts */}
         {/* <div className='py-3' data-aos="fade-up" data-aos-duration="2000">
@@ -335,7 +335,7 @@ Challenges of Microservices </Link></div></div>
             <input className="inputBoxFlex" placeholder="&nbsp;&nbsp;Phone" width="90%" />
           </div>
           <div className="col-sm-4 p-2 " >
-            
+
             <select className="inputBoxFlex">
               <option value="enterprise">&nbsp;Contact&nbsp;Source</option>
               <option value="enterprise">&nbsp;Advertisement</option>
@@ -351,7 +351,7 @@ Challenges of Microservices </Link></div></div>
             </select>
           </div>
           <div className="col-sm-4 p-2" >
-            
+
             <select className="inputBoxFlex">
               <option value="enterprise">&nbsp;Select&nbsp;Service</option>
               <option value="enterprise">&nbsp;Enterprise&nbsp;Transformation</option>
